0cdb5a3633a71136c0b1f354ab3c6a2cba51acc3
amaliaks123 <amaliaks@ece.auth.gr>
2025-05-19 11:52:19 +0300

    Update current_sensor.ino

--- current_sensor.ino
+++ current_sensor.ino
@@ -1,28 +1,34 @@
-int res=13;
-const int currentPin = A0;
-int sensitivity = 190;
-int adcValue= 0;
-int offsetVoltage = 2500;
-double adcVoltage = 0;
-double currentValue = 0;
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219;
+const int ledPin = 13;  // LED on pin 13 (with 470Ω resistor)
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(res,OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
-  digitalWrite(res,HIGH);
+  
+  if (!ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (1); // Stop if sensor not found
+  }
+  
+  digitalWrite(ledPin, HIGH); // Turn on LED
+  Serial.println("INA219 Current Sensor Test");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  double current=0;
-  currentValue=0;
-  for(int i=0;i<10000;i++){
-    adcValue = analogRead(currentPin);
-    adcVoltage = (adcValue / 1024.0) * 5000;
-    currentValue = currentValue + ((adcVoltage - offsetVoltage) / sensitivity); // mA
-  }
-  current=((currentValue)/10-10);
-  Serial.println(current);
-  delay(500);
+  float shuntVoltage_mV = ina219.getShuntVoltage_mV();
+  float busVoltage_V = ina219.getBusVoltage_V();
+  float current_mA = ina219.getCurrent_mA();
+  float power_mW = ina219.getPower_mW();
+  float loadVoltage_V = busVoltage_V + (shuntVoltage_mV / 1000);
+
+  // Print measurements
+  Serial.print("Load Voltage: "); Serial.print(loadVoltage_V); Serial.println(" V");
+  Serial.print("Current: "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.print("Power: "); Serial.print(power_mW); Serial.println(" mW");
+  Serial.println("-----------------------");
+  
+  delay(1000); // Update every second
 }
